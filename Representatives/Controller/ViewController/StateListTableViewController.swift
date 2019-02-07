25128c135f45aff2918697ab7fb0448dfffa79ac
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-07 15:24:01 -0700

    Adds StateListViewController

---
+++ Representatives/Controller/ViewController/StateListTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  StateListTableViewController.swift
+//  Representatives
+//
+//  Created by Dominic Lanzillotta on 2/7/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class StateListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return States.all.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "stateCell", for: indexPath)
+
+        cell.textLabel?.text = States.all[indexPath.row]
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
