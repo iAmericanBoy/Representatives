b9a0f96fe6de701d2e0117f7a9951e7e739abffc
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-07 14:02:29 -0700

    Adds Model

---
+++ Representatives/Model/Representative.swift
@@ -0,0 +1,18 @@
+//
+//  Representative.swift
+//  Representatives
+//
+//  Created by Dominic Lanzillotta on 2/7/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import Foundation
+
+struct Representative: Codable {
+    let name: String
+    let party: String
+    let state: String
+    let phone: String
+    let office: String
+    let link: String
+}
